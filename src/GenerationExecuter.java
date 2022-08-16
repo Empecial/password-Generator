a9498638de89994df30575a945bd2743c3110f8f
Empecial <empecial@gmail.com>
2022-08-16 23:56:28 +0200

    calling strong password

--- src/GenerationExecuter.java
+++ src/GenerationExecuter.java
@@ -1,7 +1,13 @@
 public class GenerationExecuter {
     public static void main(String[] args) {
-        PasswordGeneration myPass = new PasswordGeneration(20);
+        PasswordGeneration myPass = new PasswordGeneration(8);
         myPass.generate();
         System.out.println(myPass.getPassword());
+
+        //How do you do Strong password generation?
+        PasswordGeneration myStrongPass = new StrongerPassGenerator(10);
+        myStrongPass.generate();
+        System.out.println(myStrongPass.getPassword());
+
     }
 }
